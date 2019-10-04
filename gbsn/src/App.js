03d07d9c8c62fc5534d5c4a83375c53875f7dd8a
José L. S. A <alagunasalahaddin@live.com>
2019-10-04 15:45:39 +0300

    [GBSN] :fire: Update index html and App.

--- gbsn/src/App.js
+++ gbsn/src/App.js
@@ -3,6 +3,16 @@ import React from 'react';
 function App() {
   return (
     <div className="App brand-prestigio">
+      <div class="container-fluid prestigio-brand-background">
+        <div class="row pr-close-row">
+          <div class="col col-lg-6 offset-lg-3">
+            <div class="row no-gutters">
+              <div class="col col-10 col-lg-10 col-md-8 col-sm-8 offset-1 offset-lg-1 offset-md-2 offset-sm-2 prestigio-white-stripe prestigio-shadow">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
