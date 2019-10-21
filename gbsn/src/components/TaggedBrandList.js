a8f522a2f36145fd9eef5eb4645dfeebdb0f550d
José L. S. A <alagunasalahaddin@live.com>
2019-10-21 13:29:59 +0300

    [GBSN] :fire: trimmed tagged brand list component to maximum 2 elements.

--- gbsn/src/components/TaggedBrandList.js
+++ gbsn/src/components/TaggedBrandList.js
@@ -5,14 +5,24 @@ function TaggedBrandList ({mentions}) {
     console.log('====================================');
     console.log(mentions);
     console.log('====================================');
+    const brand_number=mentions.length;
     return(
         <div className="mb-2">
             {
-                mentions.map(brand=>(
+                mentions.slice(0,2).map(brand=>(
                     <TaggedBrand
                     brand={brand}
                     key={brand._id}/>
                 ))
+            }
+            {
+                brand_number > 2 &&
+                <div className="profile-timeline-tag mr-0 mb-0 d-flex align-items-center prestigio-shadow" data-toggle="modal" data-target="#moreTagsModal">
+                    <div className="prestigio-thumbnail mr-2" style={{background: '#eee'}}>
+                        <div className="thumbnail-number">+{brand_number-2}</div>
+                    </div>
+                    <p className="mb-0 small-text ellipsis">+{brand_number-2} more tags</p>
+                </div>
             }
         </div>
     )    
