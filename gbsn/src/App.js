1da33f038a418cd22c5142019a5a87ea42dfc964
José L. S. A <alagunasalahaddin@live.com>
2019-11-24 01:41:59 +0300

    [GBSN] :fire: Beautified code.

--- gbsn/src/App.js
+++ gbsn/src/App.js
@@ -33,60 +33,60 @@ function App() {
     }
   }, [currentPage]);
 
-  useEffect(() => {
-    const queryAPI = async () => {
-      let brand = "";
-      const postPerPage = 4;
-      const includes = `user%2Csocial%2Cmentions.brand`;
+  const queryAPI = async () => {
+    let brand = "";
+    const postPerPage = 4;
+    const includes = `user%2Csocial%2Cmentions.brand`;
 
-      const baseURL = `https://adcaller.com/`;
-      const brandURL = `${baseURL}brands`;
-      // const userBaseURL = `${baseURL}users`
-      const sidBrandURL = `${brandURL}?qField=brand_name&qValue=${query}`;
-      //https://adcaller.com/brands?qField=brand_name&qValue=me
+    const baseURL = `https://adcaller.com/`;
+    const brandURL = `${baseURL}brands`;
+    // const userBaseURL = `${baseURL}users`
+    const sidBrandURL = `${brandURL}?qField=brand_name&qValue=${query}`;
+    //https://adcaller.com/brands?qField=brand_name&qValue=me
 
-      if (query === "") return;
-      else if (query === "me") brand = "Z2EfoOUFQJVs39lg";
+    if (query === "") return;
+    else if (query === "me") brand = "Z2EfoOUFQJVs39lg";
+    else {
+      // Getting the sidbrand in order to get the brand
+      // TODO: Improve the brand searching method. but it depends on the API.
+      const searchBrand = await fetch(sidBrandURL);
+      const searchBrandResult = await searchBrand.json();
+
+      if (searchBrandResult.data.attributes[0])
+        brand = searchBrandResult.data.attributes[0].sidbrand;
       else {
-        // Getting the sidbrand in order to get the brand
-        // TODO: Improve the brand searching method. but it depends on the API.
-        const searchBrand = await fetch(sidBrandURL);
-        const searchBrandResult = await searchBrand.json();
-
-        if (searchBrandResult.data.attributes[0]) brand = searchBrandResult.data.attributes[0].sidbrand;
-        else {
-          saveError(true);
-          return;
-        }
-        // if it cannot get the result, raise an error
+        saveError(true);
+        return;
       }
+      // if it cannot get the result, raise an error
+    }
 
-      const url = `${brandURL}/${brand}/mentioned_social_posts?limit=${postPerPage}&includes=${includes}&page=${currentPage}`;
-      /* https://adcaller.com/brands/Z2EfoOUFQJVs39lg/mentioned_social_posts?page=2&limit=4&includes=social%2Cmentions.brand*/
+    const url = `${brandURL}/${brand}/mentioned_social_posts?limit=${postPerPage}&includes=${includes}&page=${currentPage}`;
+    /* https://adcaller.com/brands/Z2EfoOUFQJVs39lg/mentioned_social_posts?page=2&limit=4&includes=social%2Cmentions.brand*/
+    const answer = await fetch(url);
+    const result = await answer.json();
 
-      const answer = await fetch(url);
-      const result = await answer.json();
+    // TODO: Get top mentioner list
+    let topMentionerList = result.data.attributes.reduce((r, a) => {
+      r[a.usersid] = [...(r[a.usersid] || []), a];
+      r[a.usersid]["name"] = a.user[0].userinfo.displayname;
+      r[a.usersid]["key"] = a.usersid;
+      r[a.usersid]["picture"] = a.user[0].userinfo.avatar_url;
+      // TODO: just add increase a counter and add the name and photo url
+      return r;
+    }, {});
+    // TODO: sort in descendent order
 
-      // TODO: Get top mentioner list
-      let topMentionerList = result.data.attributes.reduce((r, a) => {
-        r[a.usersid] = [...(r[a.usersid] || []), a];
-        r[a.usersid]["name"] = a.user[0].userinfo.displayname;
-        r[a.usersid]["key"] = a.usersid;
-        r[a.usersid]["picture"] = a.user[0].userinfo.avatar_url;
-        // TODO: just add increase a counter and add the name and photo url
-        return r;
-      }, {});
-      // TODO: sort in descendent order
+    if (!result.data.attributes.length) {
+      setPostsLoaded(true);
+    }
 
-      console.log("====================================");
-      console.log(topMentionerList);
-      console.log("====================================");
+    saveError(false);
+    setIsFetching(false);
+    saveMentionedSocialPost(m => [...m, ...result.data.attributes]);
+    saveTopMentioners(Object.values(topMentionerList));
+  };
 
-      saveError(false);
-      saveMentionedSocialPost(m => [...m, ...result.data.attributes]);
-      saveTopMentioners(Object.values(topMentionerList));
-    };
-    queryAPI();
   }, [query, currentPage]);
 
   return (
@@ -114,7 +114,9 @@ function App() {
                   </div>
                   {isFetching && "Fetching more list items..."}
                 </div>
-                {error ? <Error message="No brand was matched with your query!" /> : null}
+                {error ? (
+                  <Error message="No brand was matched with your query!" />
+                ) : null}
               </div>
             </div>
           </div>
